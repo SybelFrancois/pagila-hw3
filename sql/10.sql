19bb1c87214536aa65f34e11ac689ba9396c299e
Sybel Francois <123044356+SybelFrancois@users.noreply.github.com>
2023-03-11 14:08:33 -0800

    Update 10.sql

--- sql/10.sql
+++ sql/10.sql
@@ -1,10 +1,14 @@
 /*
- * Management wants to rank customers by how much money they have spent in order to send coupons to the top 10%.
+ * Management wants to rank customers by how much money they have spent in 
+ order to send coupons to the top 10%.
  * The query below computes the total amount that each customer has spent.
- * It needs to be modified so that the "percentile" column contains the customer's percentile, and then filtered so that only customers with percentile >= 90 are kept.
+ * It needs to be modified so that the "percentile" column contains the customer's 
+ percentile, and then filtered so that only customers with percentile >= 90 are kept.
  *
- * Like the previous problem, this problem requires that you learn a new weird syntax: window functions.
- * There are many window functions in postgres, but for this assignment you'll want to use the ntile function (with parameter n=100).
+ * Like the previous problem, this problem requires that you learn a new weird syntax:
+ window functions.
+ * There are many window functions in postgres, but for this assignment you'll want to
+ use the ntile function (with parameter n=100).
  * See the following tutorials for a guide:
  * 1. https://www.postgresqltutorial.com/postgresql-ntile-function/
  * 2. https://www.postgresqltutorial.com/postgresql-window-function/
