1b4e72621ceb6e8babf112a0d375bbc41eab48b0
Sybel Francois <123044356+SybelFrancois@users.noreply.github.com>
2023-03-11 14:05:57 -0800

    Update 05.sql

--- sql/05.sql
+++ sql/05.sql
@@ -1,7 +1,9 @@
 /*
- * You love the acting in the movie 'AMERICAN CIRCUS' and want to watch other movies with the same actors.
+ * You love the acting in the movie 'AMERICAN CIRCUS' and want to watch 
+ other movies with the same actors.
  *
- * Write a SQL query that lists the title of all movies where at least 2 actors were also in 'AMERICAN CIRCUS'.
+ * Write a SQL query that lists the title of all movies where at least 2
+ actors were also in 'AMERICAN CIRCUS'.
  *
  * HINT:
  * This can be solved with a self join on the film_actor table.
