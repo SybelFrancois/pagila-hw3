40c4c3ecb6d67427886e48079c978073b9c374a0
Sybel Francois <123044356+SybelFrancois@users.noreply.github.com>
2023-03-11 14:04:59 -0800

    Update 01.sql

--- sql/01.sql
+++ sql/01.sql
@@ -4,5 +4,8 @@
 
 
  select count(customer_id) from customer where address_id not in (
-    select distinct address.address_id from address join city using (city_id) join country using (country_id) where country.country = 'United States' or country.country = 'US'
+    select distinct address.address_id from address 
+       join city using (city_id) 
+       join country using (country_id) 
+       where country.country = 'United States' or country.country = 'US'
 );
