dab49740c642ad25c50e6674edcf9f8360937c8c
Sybel Francois <123044356+SybelFrancois@users.noreply.github.com>
2023-03-11 14:07:06 -0800

    Update 08.sql

--- sql/08.sql
+++ sql/08.sql
@@ -5,17 +5,20 @@
  * Your idea is that if a lot of people have rented both 'BUCKET BROTHERHOOD' and movie X,
  * then movie X must be similar and something you'd like to watch too.
  * Your goal is to create a SQL query that finds movie X.
- * Specifically, write a SQL query that returns all films that have been rented by at least 3 customers who have also rented 'BUCKET BROTHERHOOD'.
+ * Specifically, write a SQL query that returns all films that have been rented by at 
+ least 3 customers who have also rented 'BUCKET BROTHERHOOD'.
  *
  * HINT:
  * This query is very similar to the query from problem 06,
  * but you will have to use joins to connect the rental table to the film table.
  *
  * HINT:
- * If your query is *almost* getting the same results as mine, but off by 1-2 entries, ensure that:
+ * If your query is *almost* getting the same results as mine, but off by 1-2 entries, 
+ ensure that:
  * 1. You are not including 'BUCKET BROTHERHOOD' in the output.
  * 2. Some customers have rented movies multiple times.
- *    Ensure that you are not counting a customer that has rented a movie twice as 2 separate customers renting the movie.
+ *    Ensure that you are not counting a customer that has rented a movie twice as 2 
+ separate customers renting the movie.
  *    I did this by using the SELECT DISTINCT clause.
  */
 
